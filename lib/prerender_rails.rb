ee8bf178f66cab739e90b1b1a136364521c45f02
Dirk Coetzer <dirkcoetzer@gmail.com>
2017-09-22 15:53:32 +0200

    Set the ports to nil to avoid them being sent to prerender

--- lib/prerender_rails.rb
+++ lib/prerender_rails.rb
@@ -219,7 +219,7 @@ module Rack
         new_env["X_FORWARDED_PROTO"] = "http" and new_env["X_FORWARDED_PORT"] = 80 and new_env["HTTPS"] = false and new_env["rack.url_scheme"] = "http" and new_env["SERVER_PORT"] = 80 if @options[:protocol] == "http"
       end
 
-      url = Rack::Request.new(new_env).url
+      url = Rack::Request.new(new_env).url.to_s.sub(/:\d+\z/, '')
       prerender_url = get_prerender_service_url()
       forward_slash = prerender_url[-1, 1] == '/' ? '' : '/'
 
