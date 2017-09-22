3e980beec15e25feea59d5339992cf0a2e0d0c7e
Dirk Coetzer <dirkcoetzer@gmail.com>
2017-09-22 10:54:32 +0200

    Print the prerender url for debugging.

--- lib/prerender_rails.rb
+++ lib/prerender_rails.rb
@@ -221,6 +221,8 @@ module Rack
       prerender_url = get_prerender_service_url()
       forward_slash = prerender_url[-1, 1] == '/' ? '' : '/'
 
+      Rails.logger.debug "#{self.class.name.to_s}::#{__method__}"
+
       "#{prerender_url}#{forward_slash}#{url}"
     end
 
